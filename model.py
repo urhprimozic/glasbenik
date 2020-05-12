a8ee48ba0cf35b8bfe044bf7569cf74c2727f87d
urh <urh.primozic@gmail.com>
2020-05-12 13:42:42 +0200

    Predvajanje iz urlja Dodal funckijo predvajaj_url, ki s pomočjo vlcja predvaja video.

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # $ Z $ označim komentarji, ki morajo stran PRED ZADNJO VERZIJO
 # $PAZI:
 # $Model ne sme vedet za vmesnik
@@ -6,7 +7,7 @@ import vlc
 import pafy
 
 
-# $TO DO: Združi te razrede!??
+# $TODO: Združi te razrede!??
 class Predvajalnik:
     '''
     Poskrbi za magijo z moduloma vlc in pafy, da predvaja glasbo iz spletnega naslova.
@@ -16,7 +17,29 @@ class Predvajalnik:
 
     def __init__(self):
         self.trenutna_pesem = None
-        pass  # $bomo jutr
+
+        #zažene vlc
+        self.vlc_instance = vlc.Instance() # Običaj za poimenovanje te spremenljivkle je instance, prevod bi bil čuden
+        self.vlc_predvajalnik = self.vlc_instance.media_player_new()
+        
+
+    def predvajaj_url(self, url):
+        '''
+        Predvaja glasbo iz posnetka, ki se nahaja na spletnem naslovu url.
+        TODO: Preveri internetno povezavo, preveri url
+        '''
+        # pafy poišče najbolšo kvaliteto za prenos v živo
+        kvaliteten_url = pafy.new(url).getbest().url
+        
+        # vlc magija
+        media = self.vlc_instance.media_new(kvaliteten_url)
+        media.get_mrl()
+        self.vlc_predvajalnik.set_media(media)
+
+        # predvajaj
+        self.vlc_predvajalnik.play()
+
+
 
 
 class Lokalni_iskalnik:
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 #Ti testi niso ignorirani
 #TO DO: Dol iz finalne verzije
+import model
+
+predvajalnik = model.Predvajalnik()
+predvajalnik.predvajaj_url('https://www.youtube.com/watch?v=r7gFNaGYEs8')
