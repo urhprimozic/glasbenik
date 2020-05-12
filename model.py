0db12f8c6bdcaa51568036f6ee14a4a2a2f4e6da
urh <urh.primozic@gmail.com>
2020-05-12 10:07:53 +0200

    Dodal delovne komentarje Komentira stvari za lažji razvoj. Komentarji, začeti  z ## bodo za končno verzijo odstranjeni.

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
-# PAZI:
-# Model ne sme vedet za vmesnik
-# slovenske spremenljivke
+## Z dvojnim ## označim komentarji, ki morajo stran PRED ZADNJO VERZIJO
+## PAZI:
+## Model ne sme vedet za vmesnik
+## slovenske spremenljivke
 import vlc
 import pafy
 
 
-# TO DO: Združi te razrede!??
+## TO DO: Združi te razrede!??
 class Predvajalnik:
     """
     Poskrbi za magijo z moduloma vlc in pafy, da predvaja glasbo iz spletnega naslova.
@@ -13,9 +14,9 @@ class Predvajalnik:
         > Nič
     """
     def __init__(self):
+        self.trenutna_pesem = None
         pass #bomo jutr
 
-#Ali potrebujem poseben razred za predlaganje glasbe? Po mojem ne
 class Lokalni_iskalnik:
     """
     Glede na geslo zna iskati po lokalni bazi, mogoče tudi predlaga glasbo.
@@ -30,4 +31,4 @@ class Spletni_iskalnik:
     def __init__(self):
         pass
 
-# Trenutno stanje: Znaš predvajat muzko (predvajalnik) in znaš iskat po obeh bazah (predpostavimo, da lokalni tud predlaga stvari)
+## Trenutno stanje: Znaš predvajat muzko (predvajalnik) in znaš iskat po obeh bazah (predpostavimo, da lokalni tud predlaga stvari)
---
+++ test.py
@@ -0,0 +1,2 @@
+#Ti testi niso ignorirani
+#TO DO: Dol iz finalne verzije
