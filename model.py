68a49144a337e1abe805a68a55067c0ab67ec23a
urh <urh.primozic@gmail.com>
2020-05-12 13:06:17 +0200

    Uredil v skaldu s PEP8 Uporaba ## za komentarje je grda.

--- model.py
+++ model.py
@@ -1,34 +1,39 @@
-## Z dvojnim ## označim komentarji, ki morajo stran PRED ZADNJO VERZIJO
-## PAZI:
-## Model ne sme vedet za vmesnik
-## slovenske spremenljivke
+# $ Z $ označim komentarji, ki morajo stran PRED ZADNJO VERZIJO
+# $PAZI:
+# $Model ne sme vedet za vmesnik
+# $slovenske spremenljivke
 import vlc
 import pafy
 
 
-## TO DO: Združi te razrede!??
+# $TO DO: Združi te razrede!??
 class Predvajalnik:
-    """
+    '''
     Poskrbi za magijo z moduloma vlc in pafy, da predvaja glasbo iz spletnega naslova.
     Trenutno zna:
         > Nič
-    """
+    '''
+
     def __init__(self):
         self.trenutna_pesem = None
-        pass #bomo jutr
+        pass  # $bomo jutr
+
 
 class Lokalni_iskalnik:
-    """
+    '''
     Glede na geslo zna iskati po lokalni bazi, mogoče tudi predlaga glasbo.
-    """
+    '''
+
     def __init__(self):
         super().__init__()
 
+
 class Spletni_iskalnik:
-    """
+    '''
     Glede na geslo zna iskati po spletni platformi youtube.
-    """
+    '''
+
     def __init__(self):
         pass
 
-## Trenutno stanje: Znaš predvajat muzko (predvajalnik) in znaš iskat po obeh bazah (predpostavimo, da lokalni tud predlaga stvari)
+# $Trenutno stanje: Znaš predvajat muzko (predvajalnik) in znaš iskat po obeh bazah (predpostavimo, da lokalni tud predlaga stvari)
